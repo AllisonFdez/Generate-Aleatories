b30fceba9f48f1165b56f56b656a80c809947452
Allison <fdezlobo.al@gmail.com>
2021-05-20 18:12:23 -0200

    English version

--- Aleatories.py
+++ Aleatories.py
@@ -3,24 +3,23 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generarAleatorios(cant):
-    numeros = [random.randint(0,100) for _ in range(cant)]
+def generateAleatories(cant):
+    numbers = [random.randint(0,100) for _ in range(cant)]
     
-    archivo = 'Aleatories.json'
-    with open(archivo, 'w') as file:
-        file.write('{}'.format(numeros))
-    print(str(cant)+' Números aleatorios entre 0-100 guardados en '+archivo)
+    filename = 'Aleatories.json'
+    with open(filename, 'w') as file:
+        file.write('{}'.format(numbers))
+    print(str(cant)+' Random numbers over the range 0-100 saved in '+filename)
 
     xAxis = list(range(0,101))
-    cantidad = []
-    cantidadDict = {i:numeros.count(i) for i in xAxis}
+    duplicates = []
     
     for x in xAxis:
-        cantidad.append(numeros.count(x))
+        duplicates.append(numbers.count(x))
     
     values = np.arange(len(xAxis))
-    plt.bar(values, cantidad); 
+    plt.bar(values, duplicates); 
     plt.title("Cant. of Duplicates"); plt.xlabel("Number"); plt.ylabel("Count")
     plt.show()
 
-generarAleatorios(1000)
+generateAleatories(1000)
